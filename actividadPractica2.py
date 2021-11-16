0ba983b481a1bce3cafaf6825e64498a966de6e1
magalipujol <magalicpujol@gmail.com>
2021-11-16 10:26:01 -0300

    ejercicio 1, práctica 2

--- actividadPractica2.py
+++ actividadPractica2.py
@@ -1,1 +1,15 @@
 # %%
+# 1. Desarrollar una función que reciba tres números positivos y devuelva el mayor
+# de los tres, sólo si éste es único (mayor estricto). En caso de no existir el mayor
+# estricto devolver -1.
+def biggerNumber(*numbers):
+    listOfNumbers = list(numbers)
+    listOfNumbers.sort()
+    biggerElement = listOfNumbers[-1]
+    if listOfNumbers.count(biggerElement) == 1:
+        return biggerElement
+    else: return -1
+assert(biggerNumber(1, 5, 2, 4, 12) == 12)
+assert(biggerNumber(12, 5, 2, 4, 12) == -1)
+
+# %%
